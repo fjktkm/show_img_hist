e9d0a73a0c1d0f3b3ccebba790a877663b84a036
fjktkm <fjktkm@gmail.com>
2023-06-22 00:22:38 +0900

    画像の修正

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from skimage.io import imread
 
 
 # download the image
-img_url = 'https://pbs.twimg.com/media/FszEfbRaIAAXqba?format=jpg&name=4096x4096'
+img_url = 'https://pbs.twimg.com/media/FszEfbRaIAAXqba.jpg'
 
 im = imread(img_url)
 
